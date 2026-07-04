f4b6fe41e8000b237c9586b775c1d91505eb4531
fullmodel-star <c0787@csd.org.tw>
2026-07-04 17:00:48 +0800

    內容審查修正：刪2造字(protagonize/protagonism)+修中譯/例句/詞性19項+全形標點正規化47處；sw v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='vocab-toeic-v1';
+const C='vocab-toeic-v2';
 const A=['index.html','manifest.json','icon-192.png','icon-512.png','icon-maskable-512.png','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});
